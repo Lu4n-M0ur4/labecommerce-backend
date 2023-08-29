4fa0df84619a1ab93ade69dba43f2d44beff1692
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2023-08-29 16:17:26 -0300

    primeiro exercicio backEnd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Criação inicial');
+const initProjet = (num) => {
+   
+  for (let i = 0; i < num; i++) {
+    setTimeout(() => {
+      console.log(`iniciando em ${num - i}`);
+    }, `${i}000`);
+  }
+};
+initProjet(3)
